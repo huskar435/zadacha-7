f32a28a7e6995bbbf96df7ba3b876e5626e2e5ce
huskar435 <Godkraken@tutamail.com>
2024-10-11 17:32:56 +0300

    zadacha7-7

---
+++ main.py
@@ -0,0 +1,18 @@
+input_data = open("input.txt","r")
+data = input_data.read()
+data = data.split()
+a = int(data[0])
+b = int(data[1])
+c = int(data[2])
+d = 0
+if (a > b) and (a > c):
+    d = a
+elif (b > a) and (b > c):
+    d = b
+elif (c > a) and (c > b):
+    d = c 
+d = str(d)    
+output_data = open("output.txt","w")
+output_data.write (d)
+input_data.close()
+output_data.close()     
